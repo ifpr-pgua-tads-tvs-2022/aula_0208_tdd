0c3cb7d7f02d292363a1d44d18e230c2af72a1c0
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2022-08-02 07:30:05 -0300

    código inicial

---
+++ src/main/java/ifpr/pgua/eic/tvs/tdd/models/Lance.java
@@ -0,0 +1,29 @@
+package ifpr.pgua.eic.tvs.tdd.models;
+
+public class Lance {
+
+    private String comprador;
+    private double valor;
+    
+    public Lance(String comprador, double valor) {
+        this.comprador = comprador;
+        this.valor = valor;
+    }
+    
+    public String getComparador() {
+        return comprador;
+    }
+    public void setComparador(String comprador) {
+        this.comprador = comprador;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    
+
+    
+}
